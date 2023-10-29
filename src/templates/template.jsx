896d31d58e3465f3ae7a0721fa8202f924600205
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-10-29 11:47:43 -0500

    now we can log out

--- src/templates/template.jsx
+++ src/templates/template.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
+
 const Template = () => {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    localStorage.removeItem("userData");
+    window.location.reload();
+  }
+
   return (
     <>
       <header>
         <nav>
           <Link to="/main">Main</Link>
           <Link to="/main/movements">Movements</Link>
+          <Link onClick={handleClick}>Log out</Link>
         </nav>
       </header>
       <Outlet />
