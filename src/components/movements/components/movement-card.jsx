37b9ee0e8ba0c85516c373cd7e9a4e3655df5491
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-10-30 08:31:20 -0500

    we have changed styles

--- src/components/movements/components/movement-card.jsx
+++ src/components/movements/components/movement-card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 const MovementCard = ({ type, amount, from, date }) => {
   return (
-    <section className={`card ${type ? "green" : "red"}`}>
+    <section className={`card ${type && "gray"}`}>
       <div className="info">
         <p>{date}</p>
         <p>{from}</p>
       </div>
       <h1>
-        {type ? "+" : "-"} {amount.toLocaleString("en-US", {style:"currency", currency:"USD"})}
+        {type ? "+" : "-"}{" "}
+        {amount.toLocaleString("en-US", { style: "currency", currency: "USD" })}
       </h1>
     </section>
   );
