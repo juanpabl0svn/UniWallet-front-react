a47f3c9c1233ac114a46e825bb9594764f0af834
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-10-30 08:51:34 -0500

    stye

--- src/components/loan/page.jsx
+++ src/components/loan/page.jsx
@@ -1,10 +1,23 @@
-import './loan.css';
+import "./loan.css";
 
 const Loan = () => {
   return (
-    <main className='loan'>
+    <main className="loan">
       <h1>Loan</h1>
-      
+
+      <form>
+        <div>
+          <label htmlFor="">Motivo</label>
+          <input type="text" />
+        </div>
+
+        <div>
+          <label htmlFor="">Cantidad</label>
+          <input type="number" min={10000} max={150000} />
+        </div>
+
+        <input type="submit" value="Solicitar credito" />
+      </form>
     </main>
   );
 };
